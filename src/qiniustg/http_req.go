78cebedb54a0104cf36593cb9ac4ba057f866dcf
liangchaoboy <liang.chao.boy@gmail.com>
2018-02-06 09:05:17 +0800

    net/http

--- src/qiniustg/http_req.go
+++ src/qiniustg/http_req.go
@@ -3,13 +3,13 @@ package qiniustg
 import (
 	"config"
 	"fmt"
-	"github.com/valyala/fasthttp"
+	"net/http"
 )
 
 func HttpReq(retCh chan string, urls []string, cfg config.Config) {
 
 	for _, u := range urls {
-		code, body, err := fasthttp.Get(nil, u+cfg.FopQuery)
+		code, body, err := http.Get(nil, u+cfg.FopQuery)
 		if err != nil {
 			retCh <- fmt.Sprintf("%s\t%d\t%s", u, code, err.Error())
 		} else {
