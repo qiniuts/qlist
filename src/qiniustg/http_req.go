49bc5560bb73147ffbe8578ac69da00e8b456604
liangchaoboy <liang.chao.boy@gmail.com>
2018-02-06 09:15:59 +0800

    addReq

--- src/qiniustg/http_req.go
+++ src/qiniustg/http_req.go
@@ -3,17 +3,29 @@ package qiniustg
 import (
 	"config"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 )
 
 func HttpReq(retCh chan string, urls []string, cfg config.Config) {
 
-	for _, u := range urls {
-		code, body, err := http.Get(nil, u+cfg.FopQuery)
+	for _, key := range keys {
+		resp, err := http.Get(key + cfg.FopQuery)
 		if err != nil {
-			retCh <- fmt.Sprintf("%s\t%d\t%s", u, code, err.Error())
-		} else {
-			retCh <- fmt.Sprintf("%s\t%d\t%s", u, code, body)
+			retCh <- fmt.Sprintf("%s\t%d\t%s", key, 900, err.Error())
+			continue
 		}
+
+		defer resp.Body.Close()
+		code := resp.StatusCode
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			retCh <- fmt.Sprintf("%s\t%d\t%s", key, 900, err.Error())
+			continue
+		}
+
+		retCh <- fmt.Sprintf("%s\t%d\t%s", key, code, body)
 	}
+	return nil
+
 }
