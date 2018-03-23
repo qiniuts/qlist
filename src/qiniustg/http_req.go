00f2bb752c0b215af39f30438359cf459b1c9ecc
rwifeng <rw.ifeng@gmail.com>
2018-03-23 18:52:10 +0800

    add random ip

--- src/qiniustg/http_req.go
+++ src/qiniustg/http_req.go
@@ -4,13 +4,58 @@ import (
 	"config"
 	"fmt"
 	"io/ioutil"
+	"math/rand"
+	"net"
 	"net/http"
+	"time"
 )
 
 func HttpReq(recordsCh, retCh chan string, cfg config.Config) {
 
+	dIps := map[string][]string{}
+	getIp := func(host string) (ip string, err error) {
+
+		ips, ok := dIps[host]
+		if !ok {
+			ipEntries, err := net.LookupIP(host)
+			if err != nil {
+				return "", err
+			}
+			fmt.Println(ipEntries)
+
+			for _, ip := range ipEntries {
+				ips = append(ips, ip.String())
+			}
+
+			dIps[host] = ips
+		}
+
+		rand.Seed(time.Now().UTC().UnixNano())
+
+		fmt.Println(ips)
+
+		return ips[rand.Intn(len(ips))], nil
+	}
+	cli := http.DefaultClient
+
 	for url := range recordsCh {
-		resp, err := http.Get(url + cfg.FopQuery)
+
+		req, err := http.NewRequest("GET", url+cfg.FopQuery, nil)
+		if err != nil {
+			retCh <- fmt.Sprintf("%s\t%d\t%s", url, 900, err.Error())
+			continue
+		}
+
+		ip, err := getIp(req.Host)
+		if err != nil {
+			retCh <- fmt.Sprintf("%s\t%d\t%s", url, 900, err.Error())
+			continue
+		}
+
+		fmt.Println(ip)
+		req.URL.Host = ip
+
+		resp, err := cli.Do(req)
 		if err != nil {
 			retCh <- fmt.Sprintf("%s\t%d\t%s", url, 900, err.Error())
 			continue
